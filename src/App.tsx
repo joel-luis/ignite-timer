a0368c02539a5558bad5a7751a37d5bbb5062452
Joel Luis <joelluis73@gmail.com>
2022-09-10 16:11:53 -0300

    create theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './themes/default'
+import { GlobalStyle } from './themes/global'
+
 function App() {
-  return <h1>Vite - Bolierplate</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default App
